226f314a34757d380084b34179a808022dab426a
Ogeday Isiklar <ogeday@gmail.com>
2016-11-17 13:05:16 +0000

    tests done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var app = express()
+var users = ['John', 'Betty', 'Hal'];
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World, use api at <a href="/api/users">/api/users</a>')
+});
+
+app.get('/api/users', function (req, res) {
+  res.json(users);
+});
 
 module.exports = app
--- test/index.js
+++ test/index.js
@@ -1,15 +1,28 @@
 'use strict';
 
+var test = require('tape');
 var request = require('supertest');
 var app = require('../server');
-var test = require('tape');
 
 test('Home page returned', function (t) {
   request(app)
     .get('/')
+    .expect(200)
+    .end(function (err, res) {
+      t.end();
+    });
+});
+
+test('Correct users returned', function (t) {
+  request(app)
+    .get('/api/users')
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function (err, res) {
+      var expectedUsers = ['John', 'Betty', 'Hal'];
+
+      t.error(err, 'No error');
+      t.same(res.body, expectedUsers, 'Users as expected');
       t.end();
     });
 });
